5098a49d7d25d8a05ef5722c1b11196aa35400b6
Shivakumar P <p.sivakumar1986@gmail.com>
2019-01-21 16:01:34 +0300

    api log 1.0

--- client/server.js
+++ client/server.js
@@ -1,6 +1,6 @@
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const app            = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,125 +9,145 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Add headers
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-    // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  //res.setHeader('Access-Control-Allow-Headers', '*');
 
-    // Request headers you wish to allow
-    //res.setHeader('Access-Control-Allow-Headers', '*');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Content-Type", "application/json;charset=UTF-8");
 
-    res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-
-    // Pass to next layer of middleware
-    next();
-
+  // Pass to next layer of middleware
+  next();
 });
 
-
-app.post('/key', (req, res) => {
-
+app.post("/key", (req, res) => {
   var Request = require("request");
 
-  var mode = req.body.mode === 'Development' ? 'http://35.194.57.148:8080' : 'https://api.tap.company';
+  var mode =
+    req.body.mode === "Development"
+      ? "http://35.194.57.148:8080"
+      : "https://api.tap.company";
   //var mode = 'https://api.tap.company';
   var requestbody = req.body.reqBody ? req.body.reqBody : {};
-  var header = Object.assign({}, req.body.headers,
+  console.log("/key >> req", JSON.stringify(requestbody));
+
+  var header = Object.assign({}, req.body.headers, {
+    access_key: "ak_XKokBfNWv6FIYuTMg5sLPjhJ",
+    "Content-Type": "application/json",
+    Application:
+      "app_locale=en_UA|requirer=checkout|app_id=company.tap.checkout|requirer_os=pc|requirer_version=2.0.0|requirer_os_version=11.3"
+  });
+
+  Request.get(
     {
-      "access_key": "ak_XKokBfNWv6FIYuTMg5sLPjhJ",
-      'Content-Type': 'application/json',
-      "Application":"app_locale=en_UA|requirer=checkout|app_id=company.tap.checkout|requirer_os=pc|requirer_version=2.0.0|requirer_os_version=11.3"
-    });
-
-  Request.get({
-    "headers": req.body.headers,
-    "url": mode + "/v2/init",
-    }, (error, response) => {
-        if(error) {
-            return console.dir(error);
-        }
-        res.send(response.body);
-    });
-
+      headers: req.body.headers,
+      url: mode + "/v2/init"
+    },
+    (error, response) => {
+      if (error) {
+        return console.dir(error);
+      }
+      res.send(response.body);
+    }
+  );
 });
 
-
-app.post('/api', (req, res) => {
-
+app.post("/api", (req, res) => {
   var Request = require("request");
 
-  var mode = req.body.mode === 'Development' ? 'http://35.194.57.148:8080' : 'https://api.tap.company';
+  var mode =
+    req.body.mode === "Development"
+      ? "http://35.194.57.148:8080"
+      : "https://api.tap.company";
   // var mode = 'https://api.tap.company';
 
-  var header = Object.assign({}, req.body.headers,
-    {
-      'Content-Type': 'application/json',
-      "Application":"app_locale=en_UA|requirer=checkout|app_id=company.tap.checkout|requirer_os=pc|requirer_version=2.0.0|requirer_os_version=11.3"
-    });
+  var header = Object.assign({}, req.body.headers, {
+    "Content-Type": "application/json",
+    Application:
+      "app_locale=en_UA|requirer=checkout|app_id=company.tap.checkout|requirer_os=pc|requirer_version=2.0.0|requirer_os_version=11.3"
+  });
 
-  var requestbody = req.body.reqBody ? JSON.stringify(req.body.reqBody) : JSON.stringify({});
+  var requestbody = req.body.reqBody
+    ? JSON.stringify(req.body.reqBody)
+    : JSON.stringify({});
 
-  console.log('req', requestbody);
+  console.log("req", requestbody);
 
-  if(req.body.method.toLowerCase() === 'post'){
-    Request.post({
-    "headers": header,
-    "url": mode + req.body.path,
-    "body": requestbody}, (error, response, body) => {
-        if(error) {
-            res.send(error);
+  if (req.body.method.toLowerCase() === "post") {
+    Request.post(
+      {
+        headers: header,
+        url: mode + req.body.path,
+        body: requestbody
+      },
+      (error, response, body) => {
+        if (error) {
+          res.send(error);
         }
-        console.log('body', requestbody);
+        console.log("body", requestbody);
         res.send(response);
-    });
-  }
-  else if(req.body.method.toLowerCase() === 'get'){
-
-    Request.get({
-    "headers": header,
-    "url": mode + req.body.path}, (error, response) => {
-        if(error) {
-            res.send(error);
+      }
+    );
+  } else if (req.body.method.toLowerCase() === "get") {
+    Request.get(
+      {
+        headers: header,
+        url: mode + req.body.path
+      },
+      (error, response) => {
+        if (error) {
+          res.send(error);
         }
         res.send(response);
-    });
-  }
-  else if(req.body.method.toLowerCase() === 'put'){
-    Request.put({
-        "headers": header,
-        "url": mode + req.body.path,
-        "body": requestbody
-    }, (error, response, body) => {
-        if(error) {
+      }
+    );
+  } else if (req.body.method.toLowerCase() === "put") {
+    Request.put(
+      {
+        headers: header,
+        url: mode + req.body.path,
+        body: requestbody
+      },
+      (error, response, body) => {
+        if (error) {
           res.send(error);
         }
         res.send(response);
         console.log(response);
-    });
-
-  }
-  else if(req.body.method.toLowerCase() === 'delete'){
-
-    Request.delete({
-    "headers": header,
-    "url": mode + req.body.path}, (error, response) => {
-        if(error) {
-            res.send(error);
+      }
+    );
+  } else if (req.body.method.toLowerCase() === "delete") {
+    Request.delete(
+      {
+        headers: header,
+        url: mode + req.body.path
+      },
+      (error, response) => {
+        if (error) {
+          res.send(error);
         }
-        console.log('body', req.body.headers);
+        console.log("body", req.body.headers);
         res.send(response);
-    });
+      }
+    );
   }
 });
 /*
@@ -147,10 +167,9 @@ app.get('/api/:mode/:headers', (req, res) => {
 
 });*/
 
-
 //require('./routes')(app, {});
 
 const port = 8000;
 app.listen(port, () => {
-  console.log('We are live on ' + port);
+  console.log("We are live on " + port);
 });
