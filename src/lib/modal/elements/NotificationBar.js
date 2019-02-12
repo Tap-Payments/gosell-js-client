1a95c9362a5e40c0dcf59b6d6d8f5f31b40aa81b
kalpana <k.dhanabalan@tap.company>
2019-02-12 09:42:09 +0300

    Modal all classname changed

--- src/lib/modal/elements/NotificationBar.js
+++ src/lib/modal/elements/NotificationBar.js
@@ -111,14 +111,14 @@ class NotificationBar extends Component{
       }) : null;
 
       return(
-        <div dir={this.props.dir} className={this.state.show ? 'notification_bar notification_true' : 'notification_bar notification_false'} style={Object.assign({}, this.state.modeStyle, this.props.style)}>
+        <div dir={this.props.dir} className={this.state.show ? 'tap-payments-notification-bar notification-true' : 'tap-payments-notification-bar tap-payments-notification-false'} style={Object.assign({}, this.state.modeStyle, this.props.style)}>
 
-            {this.props.options ? <div className="tap-options" style={this.props.dir == 'rtl' ? {textAlign: 'left', left: '0'} : {textAlign: 'right', right: '0'}}>
+            {this.props.options ? <div className="tap-payments-options" style={this.props.dir == 'rtl' ? {textAlign: 'left', left: '0'} : {textAlign: 'right', right: '0'}}>
               {options}
               </div>
             : <a className="close" onClick={this.handleClose.bind(this)} title="close" style={this.props.dir == 'rtl' ? {left: '0'} : {right: '0'}}>×</a>}
 
-            <div className="notification_title" style={this.props.options ? {display: 'flex', textAlign: this.props.dir == 'rtl' ? 'right' : 'left'} : {}} onClick={this.props.onClick}>
+            <div className="tap-payments-notification-title" style={this.props.options ? {display: 'flex', textAlign: this.props.dir == 'rtl' ? 'right' : 'left'} : {}} onClick={this.props.onClick}>
               {this.props.children}
             </div>
         </div>
