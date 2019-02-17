07a12f8f6a5273b9cf86971f2f35d720a8195679
ralsabbagh <r.alsabbagh@tap.company>
2019-02-17 12:49:37 +0300

    ie scrollbar fix

--- webpack/paths.js
+++ webpack/paths.js
@@ -19,7 +19,7 @@ module.exports = {
 
 	imgsPath: 'https://goselljslib.b-cdn.net/imgs/',
 	fontsPath: 'https://goSellJSLib.b-cdn.net/fonts/',
-	serverPath: 'http://localhost:8000',
+	serverPath: 'http://192.168.8.193:8000',
 	// serverPath: 'http://192.168.8.164:8000',
 	// serverPath: 'http://192.168.1.119:8000',
 	// serverPath: 'http://35.237.168.102/api',
