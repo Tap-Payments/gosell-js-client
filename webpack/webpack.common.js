49d587da970641f3fd9e4375e6870b518982fda7
halaq <h.qutmosh@tap.company>
2019-02-20 15:13:35 +0300

    fixed the polyfill issue on IE browser

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: {
+		'babel-polyfill': ['babel-polyfill'],
     'gosell': commonPaths.entryPath,
     'gosell-demo': commonPaths.demoPath
   },
