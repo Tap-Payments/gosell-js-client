d5009df455f38c4f3fd7fc4defcdb8e8230eece1
Shivakumar P <p.sivakumar1986@gmail.com>
2019-02-21 12:49:00 +0300

    CI/CD update

--- demo/Main.js
+++ demo/Main.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Main extends Component {
-    render(){
-        return (
-          <div>
-            <h1>goSell Demos</h1>
-            <ul>
-              <li>
-                <Link to="/open-light-box-demo">LightBox & Elements</Link>
-              </li>
-              <li>
-                <Link to="/open-page-demo">Open Payment Page from JS library without charge / authorize ID (for now, it just called the old gateway)</Link>
-              </li>
-              <li>
-                <Link to="/gateway">Payment Gateway as a Page</Link>
-              </li>
-              <li>
-                <Link to="/demo">GoSellDemo _</Link>
-              </li>
-              <li>
-                <Link to="/elements-demo">GoSellElementsDemo _ </Link>
-              </li>
-
-            </ul>
-        </div>);
-    }
+  render() {
+    return (
+      <div>
+        <h1>goSell Demos</h1>
+        <ul>
+          <li>
+            <Link to="/open-light-box-demo">LightBox & Elements</Link>
+          </li>
+          <li>
+            <Link to="/open-page-demo">
+              Open Payment Page from JS library without charge / authorize ID
+              (for now, it just called the old gateway)
+            </Link>
+          </li>
+          <li>
+            <Link to="/gateway">Payment Gateway as a Page</Link>
+          </li>
+          <li>
+            <Link to="/demo">GoSellDemo</Link>
+          </li>
+          <li>
+            <Link to="/elements-demo">GoSellElementsDemo</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Main;
