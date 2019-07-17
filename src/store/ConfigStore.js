11c5d02aa3f4e2c9e34d92fe668d4ae39f775792
halaq <h.qutmosh@tap.company>
2019-07-17 18:30:22 +0300

    tracking header - didn’t tested on all devices yet

--- src/store/ConfigStore.js
+++ src/store/ConfigStore.js
@@ -1,5 +1,6 @@
 import {decorate, observable} from 'mobx';
 import Paths from '../../webpack/paths';
+import AppConfig from './AppConfig.js';
 
 class ConfigStore {
 
@@ -36,6 +37,18 @@ class ConfigStore {
     this.notifications = 'standard';
     // this.legalConfig = true;
     this.redirect_url = null;
+
+    this.app = {
+      app_locale:"en_UA",
+      requirer:"web.checkout",
+      app_id: "gosell.checkout.web",
+      app_client_version: "1.4.2",
+      app_server_version: "1.4.2",
+      requirer_os: "unknown",
+      requirer_os_version: "unknown",
+      requirer_browser: "unknown",
+      requirer_browser_version: "unknown"
+    }
   }
 
   callbackFunc(data){
@@ -140,6 +153,10 @@ class ConfigStore {
 
       this.notifications = value.gateway && value.gateway.notifications ? value.gateway.notifications : 'standard';
 
+      this.setAppHeader();
+
+      this.config['app'] = this.app;
+
       var URLSearchParams = require('url-search-params');
       var urlParams = new URLSearchParams(window.location.search);
       console.log('...', urlParams.has('tap_id'));
@@ -153,6 +170,83 @@ class ConfigStore {
 
 
   }
+
+  setAppHeader(){
+
+    // var OSName = "unknown";
+    // var osVersion ="unknown";
+
+    // if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+    // if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+    // if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+    // if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+    // var nAgt = navigator.userAgent;
+    // var browserVer = navigator.appVersion;
+    // var browserName  = navigator.appName;
+    // var verOffset  = 'unknown';
+    // var fullVersion = 'unknown';
+
+    // // In Opera, the true version is after "Opera" or after "Version"
+    // if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
+    //   browserName = "Opera";
+    //   fullVersion = nAgt.substring(verOffset+6);
+    //   if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+    //     fullVersion = nAgt.substring(verOffset+8);
+    // }
+    // // In MSIE, the true version is after "MSIE" in userAgent
+    // else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
+    //   browserName = "Microsoft Internet Explorer";
+    //   fullVersion = nAgt.substring(verOffset+5);
+    // }
+    // // In Chrome, the true version is after "Chrome" 
+    // else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
+    //   browserName = "Chrome";
+    //   fullVersion = nAgt.substring(verOffset+7);
+    // }
+    // // In Safari, the true version is after "Safari" or after "Version" 
+    // else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+    //   browserName = "Safari";
+    //   fullVersion = nAgt.substring(verOffset+7);
+    //   if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+    //     fullVersion = nAgt.substring(verOffset+8);
+    // }
+    // // In Firefox, the true version is after "Firefox" 
+    // else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
+    //   browserName = "Firefox";
+    //   fullVersion = nAgt.substring(verOffset+8);
+    // }
+    // // In most other browsers, "name/version" is at the end of userAgent 
+    // else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < (verOffset=nAgt.lastIndexOf('/')) ) 
+    // {
+    //   browserName = nAgt.substring(nameOffset,verOffset);
+    //   fullVersion = nAgt.substring(verOffset+1);
+    //   if (browserName.toLowerCase()==browserName.toUpperCase()) {
+    //   browserName = navigator.appName;
+    //   }
+    // }
+
+    var app = AppConfig.ui(this);
+
+    this.app = {
+      app_locale:this.language == 'ar' ? 'ar_UA' : 'en_UA',
+      requirer:"web.checkout",
+      app_id: "gosell.checkout.web",
+      app_client_version: "1.4.2",
+      app_server_version: "1.4.2",
+      requirer_os: app.os,
+      requirer_os_version: app.osversion,
+      requirer_browser: app.browser,
+      requirer_browser_version: app.version
+    }
+
+    // console.log('apppppp AppConfig.ui()', app);
+    // console.log('apppppp', app.browser);
+    // console.log('apppppp', app.version);
+    // console.log('apppppp', app.os);
+    // console.log('apppppp', app.osversion);
+  }
+
 }
 
 function isEmpty(obj){
