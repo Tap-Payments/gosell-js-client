905233fd789676cea8970258c8e313f995f7bb27
halaq <h.qutmosh@tap.company>
2019-08-29 11:36:24 +0300

    fixed the loading issue, make it faster by loading the iframe in the background before open the modal dialog

--- src/elements/GoSell.js
+++ src/elements/GoSell.js
@@ -12,24 +12,21 @@ class GoSell extends Component {
 
     RootStore.uIStore.modalMode = 'popup';
     RootStore.uIStore.setOpenModal(true);
-    RootStore.uIStore.isLoading = true;
+    // RootStore.uIStore.isLoading = true;
 
     var body = document.getElementsByTagName("BODY")[0];
     body.classList.add('gosell-payment-gateway-open');
 
     console.log(RootStore.configStore.token);
-    setTimeout(function(){
-      var iframe = document.getElementById('gosell-gateway');
 
-      iframe.addEventListener('load', function(){
-        console.log('hey loaded!');
+    var iframe = document.getElementById('gosell-gateway');
+
+    iframe.addEventListener('load', function(){
+        console.log('hey reloaded!');
         setTimeout(function(){
           RootStore.uIStore.isLoading = false;
-        }, 500);
-      });
-
-    }, 100);
-
+        }, 100);
+    });
   }
 
   //redirect to Tap gateway from JS library without calling charge / authrorize API from merchant side
@@ -43,7 +40,7 @@ class GoSell extends Component {
     var urlParams = new URLSearchParams(window.location.search);
 
     if(urlParams.has('tap_id')){
-      RootStore.uIStore.isLoading = true;
+      // RootStore.uIStore.isLoading = true;
       RootStore.uIStore.setOpenModal(true);
 
       var body = document.getElementsByTagName("BODY")[0];
@@ -53,19 +50,6 @@ class GoSell extends Component {
       RootStore.configStore.token = urlParams.get('token');
       RootStore.uIStore.modalMode = urlParams.get('mode');
       console.log('mooooode', RootStore.uIStore.modalMode);
-
-      console.log('url token', urlParams.get('token'));
-      setTimeout(function(){
-        var iframe = document.getElementById('gosell-gateway');
-
-        iframe.addEventListener('load', function(){
-          console.log('hey loaded!');
-          setTimeout(function(){
-            RootStore.uIStore.isLoading = false;
-          }, 500);
-        });
-
-      }, 100);
 
       return true;
     }
@@ -98,6 +82,15 @@ class GoSell extends Component {
 
     GoSell.showTranxResult();
 
+    var iframe = document.getElementById('gosell-gateway');
+
+    iframe.addEventListener('load', function(){
+        console.log('hey loaded!');
+        setTimeout(function(){
+          RootStore.uIStore.isLoading = false;
+        }, 100);
+    });
+
     var self = this;
     // Create IE + others compatible event handler
     var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
@@ -126,45 +119,50 @@ class GoSell extends Component {
     var urlParams = new URLSearchParams(window.location.search);
 
     if(urlParams.has('tap_id')){
-      // var url = document.location.href;
-      // var url = url.split('?');
-      // console.log('url', url[0]);
-
       window.open(RootStore.configStore.redirect_url, '_self');
     }
 
     var body = document.getElementsByTagName("BODY")[0];
     body.classList.remove('gosell-payment-gateway-open');
+
+    //reload the iframe again after closing
+    var iframe = document.getElementById('gosell-gateway');
+
+    iframe.setAttribute('src', iframe.getAttribute('src'));
+
+    RootStore.uIStore.isLoading = true;
   }
 
-  componentWillUnMount(){
-    // Create IE + others compatible event handler
-    var eventMethod = window.removeEventListener ? "removeEventListener" : "attachEvent";
-    var eventer = window[eventMethod];
-    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+  // componentWillUnMount(){
+  //   // Create IE + others compatible event handler
+  //   var eventMethod = window.removeEventListener ? "removeEventListener" : "attachEvent";
+  //   var eventer = window[eventMethod];
+  //   var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
 
-    // Listen to message from child window
-    eventer(messageEvent,function(e) {
-      if(e.data == 'close'){
-        console.log('close');
-        RootStore.uIStore.setOpenModal(false);
-        var body = document.getElementsByTagName("BODY")[0];
-        body.classList.remove('gosell-payment-gateway-open');
-      }
+  //   // Listen to message from child window
+  //   eventer(messageEvent,function(e) {
+  //     if(e.data == 'close'){
+  //       console.log('close');
+  //       RootStore.uIStore.setOpenModal(false);
+  //       var body = document.getElementsByTagName("BODY")[0];
+  //       body.classList.remove('gosell-payment-gateway-open');
+  //     }
 
-    },false);
-  }
+  //   },false);
+  // }
 
   render() {
+    console.log('client open modal', RootStore.uIStore.openModal);
+    console.log('client is loading? ', RootStore.uIStore.isLoading);
 
     return(
         <React.Fragment>
-              {RootStore.uIStore.openModal ?
+              {/* {!RootStore.uIStore.isLoading ? */}
                 <React.Fragment>
                   <iframe
                     id="gosell-gateway"
                     style={{
-                        display: !RootStore.uIStore.isLoading ? 'block' : 'none',
+                        display: RootStore.uIStore.openModal && !RootStore.uIStore.isLoading ? 'block' : 'none',
                         position: 'absolute',
                         top: '0',
                         bottom: '0',
@@ -178,7 +176,8 @@ class GoSell extends Component {
                       Paths.framePath +"?mode="+RootStore.uIStore.modalMode+"&token="+RootStore.configStore.token+"&tap_id="+ RootStore.uIStore.tap_id
                       : Paths.framePath + "?mode="+RootStore.uIStore.modalMode+"&token="+RootStore.configStore.token
                     } width="100%" height="100%"></iframe>
-
+                    
+                  {RootStore.uIStore.openModal ?
                     <TapLoader
                       type='loader'
                       color={RootStore.uIStore.modalMode === 'popup' ? 'white' : 'black'}
@@ -188,9 +187,9 @@ class GoSell extends Component {
                       title={null}
                       desc={null}
                     />
-
+                    : null}
                   </React.Fragment>
-              : null}
+               {/* : null} */}
         </React.Fragment>
       );
 
