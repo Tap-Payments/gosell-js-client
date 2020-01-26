8b3388fd5b0126db26a5fbb7b9637d3b8e0525c2
halaq <h.qutmosh@tap.company>
2020-01-26 12:15:53 +0300

    v1.5.2 -beta: solved the goSellElement issues (stop printing the token in notification and remove the notification on change or any new click)

--- src/elements/GoSellElements.js
+++ src/elements/GoSellElements.js
@@ -1,15 +1,14 @@
-import React, { Component }  from 'react';
-import {observer} from 'mobx-react';
-import NotificationBar from './NotificationBar';
-import RootStore from '../store/RootStore.js';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import NotificationBar from "./NotificationBar";
+import RootStore from "../store/RootStore.js";
 
 class GoSellElements extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
-  static async submit(){
+  static async submit() {
     await RootStore.formStore.generateToken();
   }
 
@@ -17,40 +16,41 @@ class GoSellElements extends Component {
     this.config(this.props);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     RootStore.apiStore.getLocalization().then(result => {
-       RootStore.localizationStore.strings =  result;
-       RootStore.localizationStore.isLoading = false;
+      RootStore.localizationStore.strings = result;
+      RootStore.localizationStore.isLoading = false;
     });
 
-    RootStore.formStore.generateCardForm('gosell-gateway-element-container');
+    RootStore.formStore.generateCardForm("gosell-gateway-element-container");
   }
 
   componentWillReceiveProps(nextProps) {
     this.config(nextProps);
   }
 
-  config(props){
+  config(props) {
     RootStore.configStore.setConfig(props);
   }
 
-  closeNotification(){
+  closeNotification() {
     RootStore.uIStore.getErrorHandler = {};
   }
 
   render() {
+    return (
+      <React.Fragment>
+        {RootStore.uIStore.generateCustomNotification}
 
-    return(
-        <React.Fragment>
-            {RootStore.uIStore.generateCustomNotification}
-
-            <form id="gosell-gateway-form-container" method="post" ref={(node) => this.cardElementsRef = node}>
-                <div id="gosell-gateway-element-container"></div>
-            </form>
-
-          </React.Fragment>
-      );
-
+        <form
+          id="gosell-gateway-form-container"
+          method="post"
+          ref={node => (this.cardElementsRef = node)}
+        >
+          <div id="gosell-gateway-element-container"></div>
+        </form>
+      </React.Fragment>
+    );
   }
 }
 
--- src/store/UIStore.js
+++ src/store/UIStore.js
@@ -1,105 +1,115 @@
-import React, { Component }  from 'react';
-import {decorate, observable, computed} from 'mobx';
-import NotificationBar from '../elements/NotificationBar';
+import React, { Component } from "react";
+import { decorate, observable, computed } from "mobx";
+import NotificationBar from "../elements/NotificationBar";
 
 class UIStore {
-
   constructor(RootStore) {
     this.RootStore = RootStore;
     this.reset();
   }
 
-  reset(){
-
+  reset() {
     this.tap_id = null;
     this.openModal = false;
     this.modalID = "gosell-lightbox-payment-gateway";
-    this.modalMode = 'popup';
+    this.modalMode = "popup";
     this.errorHandler = {};
 
     this.isLoading = true;
-
   }
 
-  setOpenModal(value){
+  setOpenModal(value) {
     this.openModal = value;
   }
 
-  computed
-  get generateCustomNotification(){
-
+  computed;
+  get generateCustomNotification() {
     var self = this;
     //console.log('notifications type', this.RootStore.configStore.notifications);
     //console.log('msg', this.getErrorHandler);
 
-    if(this.RootStore.configStore.notifications !== 'standard' && !this.getErrorHandler.options){
-        ////console.log('id', this.RootStore.configStore.notifications);
+    if (
+      this.RootStore.configStore.notifications !== "standard" &&
+      !this.getErrorHandler.options
+    ) {
+      ////console.log('id', this.RootStore.configStore.notifications);
 
-        var el = document.getElementById(this.RootStore.configStore.notifications);
-        ////console.log('element', el);
+      var el = document.getElementById(
+        this.RootStore.configStore.notifications
+      );
+      ////console.log('element', el);
 
-        if(this.getErrorHandler.msg && el != null){
-          ////console.log('this is happening');
-          el.innerHTML = this.getErrorHandler.msg;
-        }
-        setTimeout(function(){
-          self.closeNotification();
-        }, 5000);
-
+      if (this.getErrorHandler.msg && el != null) {
+        ////console.log('this is happening');
+        el.innerHTML = this.getErrorHandler.msg;
       }
-      else if(this.RootStore.configStore.notifications === 'standard' || this.getErrorHandler.options){
-        return(
-          <NotificationBar
-            mode={this.getErrorHandler.type}
-            dir={this.dir}
-            close={this.closeNotification}
-            show={this.getErrorHandler.visable}
-            options={this.getErrorHandler.options}>
-              {this.getErrorHandler.msg}
-          </NotificationBar>
-        );
-      }
-  }
-
-  closeNotification(){
-    var self = this;
-
-    if(self.delete_card === null && self.errorHandler.options == null) {
-      this.errorHandler.visable = false;
-      setTimeout(function(){
-        self.errorHandler = {};
-      },
-      500);
+      // setTimeout(function() {
+      //   self.closeNotification();
+      // }, 5000);
+    } else if (
+      this.RootStore.configStore.notifications === "standard" ||
+      this.getErrorHandler.options
+    ) {
+      return (
+        <NotificationBar
+          mode={this.getErrorHandler.type}
+          dir={this.dir}
+          close={this.closeNotification}
+          show={this.getErrorHandler.visable}
+          options={this.getErrorHandler.options}
+        >
+          {this.getErrorHandler.msg}
+        </NotificationBar>
+      );
     }
   }
 
-  computed
-  get getErrorHandler(){
+  // closeNotification() {
+  //   var self = this;
+  //   // if(self.delete_card === null && self.errorHandler.options == null) {
+  //   this.errorHandler.visable = false;
+  //   setTimeout(function() {
+  //     self.errorHandler = {};
+  //   }, 500);
+  //   // }
+  // }
+
+  closeNotification() {
+    var el = document.getElementById(this.RootStore.configStore.notifications);
+    el.innerHTML = null;
+    this.errorHandler.visable = false;
+    this.errorHandler = {};
+  }
+
+  computed;
+  get getErrorHandler() {
     return this.errorHandler;
   }
 
-  setErrorHandler(value){
+  setErrorHandler(value) {
     var self = this;
     this.errorHandler = value;
 
-    if(this.RootStore.configStore.notifications === 'standard' && value.visable){
+    if (
+      this.RootStore.configStore.notifications === "standard" &&
+      value.visable
+    ) {
       window.scroll(0, 0);
     }
 
-    setTimeout(function(){
-        self.closeNotification();
-    }, 5000);
+    // setTimeout(function() {
+    //   self.closeNotification();
+    // }, 5000);
   }
-
 }
 
 decorate(UIStore, {
   openModal: observable,
-  modalMode:observable,
+  modalMode: observable,
   modalID: observable,
   tap_id: observable,
-  errorHandler:observable,
-  isLoading:observable
+  errorHandler: observable,
+  isLoading: observable
 });
 
 export default UIStore;
